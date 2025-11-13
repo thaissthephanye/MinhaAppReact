18d9ba210a8bbb3e07c8d1073878fcd023e7a5c5
thais <thaissthephanye@gmail.com>
2025-11-13 13:49:44 -0300

    fist commit

---
+++ src/views/Home.jsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Main from '../components/Main'
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default Home

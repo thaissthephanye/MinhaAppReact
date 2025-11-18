19dfb1e0fa2fb4b814fbe70efff0701227bdbde8
thais <thaissthephanye@gmail.com>
2025-11-18 10:22:09 -0300

    feat: header e carrossel do conteúdo principal criados

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { PiStudentDuotone } from "react-icons/pi";
+import './Header.css';
 
-const Header = () => {
+function Header() {
   return (
-    <div>
-      Header
-    </div>
-  )
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+
+        <Navbar.Brand href="#home"><PiStudentDuotone className='distanciaLogo' />Censo Escolar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Instituições de Ensino</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { Container } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
+import './Main.css';
 
-const Main = () => {
+function Main() {
   return (
-    <div>
-      Main
-    </div>
-  )
+    <Container>
+      <Carousel className='carrossel'>
+        <Carousel.Item interval={1000}>
+          <Image className='imagem-carrossel' src="../src/assets/crianca-aluno.jpg" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <Image className='imagem-carrossel' src="../src/assets/aluna.jpg" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <Image className='imagem-carrossel' src="../src/assets/alunos-sala-aula.jpg" />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
+  );
 }
 
-export default Main
+export default Main;
